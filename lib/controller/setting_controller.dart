3a6520540f586a8827bb50b0da0e71821ffbee15
MohamedHassan139 <mohamedhessan139@gmail.com>
2023-07-29 22:47:56 +0300

    work on edit profile

--- lib/controller/setting_controller.dart
+++ lib/controller/setting_controller.dart
@@ -1,14 +1,73 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../helper/cashe_helper.dart';
 import '../model/user_model.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class SettingController extends GetxController {
   late UserDataModel model;
   String? uesrId;
   CollectionReference? users;
   DocumentReference? doc;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController bioController = TextEditingController();
+  final ImagePicker picker = ImagePicker();
+  XFile? backgroundImage;
+  File? profileImage;
+  final storage = FirebaseStorage.instance;
+  int rand = Random().nextInt(9999999);
+
+// Pick an image.
+  Future<void> getbackgroundImage() async {
+    final pickerFile = picker.pickImage(source: ImageSource.gallery);
+    if (pickerFile != null) {
+      pickerFile.then((value) {
+        backgroundImage = value;
+        update();
+      });
+    } else {
+      print('No Image Selected');
+    }
+  }
+
+  Future<void> getprofileImage() async {
+    final pickerFile = picker.pickImage(source: ImageSource.gallery);
+    if (pickerFile != null) {
+      pickerFile.then((value) {
+        profileImage = File( value!.path);
+        update();
+      });
+    } else {
+      print('No Image Selected');
+    }
+  }
+
+  Future<void> uploadProfileImage() async {
+    if (profileImage != null) {
+      print('@@@@@@@@@@@@@@@@@@@@@@@@#####');
+      print(rand);
+
+      storage
+          .ref()
+          .child('users')
+          .child(uesrId!)
+          .child('$rand${Uri.file(profileImage!.path).pathSegments.last}')
+          .putFile(profileImage!)
+          .then((value) {
+        print('Upload image success');
+      }).catchError((error) {
+        print('@@@@@@@@@@@@@@@@@@@@@@@@');
+        print(error.toString());
+        print('@@@@@@@@@@@@@@@@@@@@@@@@');
+      });
+    }
+  }
 
   @override
   void onInit() {
--- lib/view/screens/edit_profile_screen.dart
+++ lib/view/screens/edit_profile_screen.dart
@@ -1,16 +1,22 @@
+import 'dart:io';
+import 'dart:html' as html;
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import '../../controller/setting_controller.dart';
+import '../../model/user_model.dart';
 import '../widgets/custom_textformfield.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class EditeProfile extends StatelessWidget {
-  const EditeProfile({Key? key}) : super(key: key);
+  EditeProfile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     SettingController controller = Get.put(SettingController());
-    TextEditingController nameController = TextEditingController();
-    TextEditingController bioController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -20,6 +26,7 @@ class EditeProfile extends StatelessWidget {
         actions: [
           TextButton(
               onPressed: () {
+                controller.uploadProfileImage();
                 // TODO
               },
               child: const Text('Update')),
@@ -28,102 +35,144 @@ class EditeProfile extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(children: [
-          Container(
-            height: 190,
-            child: Stack(
-              alignment: AlignmentDirectional.bottomCenter,
-              children: [
-                // background image
-                Align(
-                  alignment: AlignmentDirectional.topCenter,
-                  child: Stack(
-                    alignment: AlignmentDirectional.topEnd,
-                    children: [
-                      Container(
-                        height: 140,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            topRight: Radius.circular(5),
-                          ),
-                          image: DecorationImage(
-                            image: NetworkImage('${controller.model.caver}'),
-                            fit: BoxFit.cover,
+      body: FutureBuilder<DocumentSnapshot>(
+        future: controller.doc?.get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            controller.model = UserDataModel.fromJson(
+                snapshot.data!.data() as Map<String, dynamic>);
+            controller.nameController.text = controller.model.name;
+            controller.bioController.text = controller.model.bio!;
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(children: [
+                GetBuilder<SettingController>(
+                  builder: (c) => Container(
+                    height: 190,
+                    child: Stack(
+                      alignment: AlignmentDirectional.bottomCenter,
+                      children: [
+                        // background image
+                        Align(
+                          alignment: AlignmentDirectional.topCenter,
+                          child: Stack(
+                            alignment: AlignmentDirectional.topEnd,
+                            children: [
+                              Container(
+                                height: 140,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(5),
+                                    topRight: Radius.circular(5),
+                                  ),
+                                  image: DecorationImage(
+                                    image: controller.backgroundImage != null
+                                        ? FileImage(File(
+                                            controller.backgroundImage!.path))
+                                        : NetworkImage(
+                                                '${controller.model.caver}')
+                                            as ImageProvider,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: CircleAvatar(
+                                  radius: 20,
+                                  child: IconButton(
+                                    onPressed: () {
+                                      controller.getbackgroundImage();
+                                    },
+                                    icon: const Icon(
+                                      Icons.camera_alt_outlined,
+                                      size: 16,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: CircleAvatar(
-                          radius: 20,
-                          child: IconButton(
-                            onPressed: () {
-                              // TODO
-                            },
-                            icon: const Icon(
-                              Icons.camera_alt_outlined,
-                              size: 16,
+
+                        // profile image
+                        Stack(
+                          alignment: AlignmentDirectional.bottomEnd,
+                          children: [
+                            Container(
+                              width: 120,
+                              height: 120,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color:
+                                    Theme.of(context).scaffoldBackgroundColor,
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: CircleAvatar(
+                                  radius: 30,
+                                  backgroundImage: controller.profileImage !=
+                                          null
+                                      ? FileImage(controller.profileImage!)
+                                      : NetworkImage(
+                                          '${controller.model.image}',
+                                        ) as ImageProvider,
+                                ),
+                              ),
                             ),
-                          ),
+                            CircleAvatar(
+                              radius: 20,
+                              child: IconButton(
+                                onPressed: () {
+                                  controller.getprofileImage();
+                                },
+                                icon: const Icon(
+                                  Icons.camera_alt_outlined,
+                                  size: 16,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-
-                // profile image
-                Stack(
-                  alignment: AlignmentDirectional.bottomEnd,
-                  children: [
-                    Container(
-                      width: 120,
-                      height: 120,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Theme.of(context).scaffoldBackgroundColor,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(3.0),
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: NetworkImage(
-                            '${controller.model.image}',
-                          ),
-                        ),
-                      ),
-                    ),
-                    CircleAvatar(
-                      radius: 20,
-                      child: IconButton(
-                      onPressed: () {
-                        // TODO
-                      },
-                      icon:  const Icon(
-                          Icons.camera_alt_outlined,
-                          size: 16,
-                        ),
-                      ),
-                    ),
-                  ],
+                const SizedBox(
+                  height: 20,
                 ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          CustomTextFormField(
-            hintText: 'Edite Name',
-            labelText: 'Edite Name',
-            textController: nameController,
-            fieldType: TextInputType.name,
-            prefixIcon: const Icon(Icons.person),
-          ),
-        ]),
+                CustomTextFormField(
+                  hintText: 'Edite Name',
+                  labelText: 'Edite Name',
+                  textController: controller.nameController,
+                  fieldType: TextInputType.name,
+                  prefixIcon: const Icon(Icons.person),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextFormField(
+                  hintText: 'Edite Bio',
+                  labelText: 'Edite Bio',
+                  textController: controller.bioController,
+                  fieldType: TextInputType.text,
+                  prefixIcon: const Icon(Icons.info_outlined),
+                ),
+              ]),
+            );
+          }
+          if (snapshot.hasError) {
+            return const Center(child: Text("Something went wrong..."));
+          }
+
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return const Center(child: Text("Document does not exist..."));
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
